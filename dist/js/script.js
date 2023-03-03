f3757374930120d3b3c6ba9de1d993d945e2333c
Jakub Wilga <wilga327@wp.pl>
2023-03-03 19:06:52 +0100

    Add class Cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -282,6 +282,38 @@
       thisWidget.element.dispatchEvent(event);
     }
   }
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+    initActions(){
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){ 
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+      
+      
+      
+      
+      
+    } 
+  }
 
 
   const app = {
@@ -299,6 +331,12 @@
 
       thisApp.data = dataSource;
     },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
 
     init: function(){
       const thisApp = this;
@@ -310,6 +348,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
