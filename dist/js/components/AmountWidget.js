2bbf0d188ee0ce4f403dd26c9a463520f142e856
Jakub Wilga <wilga327@wp.pl>
2023-03-16 14:01:49 +0100

    add class BaseWidget

--- dist/js/components/AmountWidget.js
+++ dist/js/components/AmountWidget.js
@@ -1,63 +1,50 @@
 import { settings, select } from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-class AmountWidget{
-  constructor(element){
+class AmountWidget extends BaseWidget {
+  constructor(element) {
+    super(element, settings.amountWidget.defaultValue);
     const thisWidget = this;
 
     thisWidget.getElements(element);
-    thisWidget.setValue(thisWidget.input.value);
-    //thisWidget.setValue(settings.amountWidget.defaultValue);
-    thisWidget.initActions();
+    thisWidget.initActions(thisWidget.value);
+  }
 
-  }
-  getElements(element){
+  getElements() {
     const thisWidget = this;
-    
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-    thisWidget.setValue(settings.amountWidget.defaultValue);
-  }
-  setValue(value){
-    const thisWidget = this;
-    const newValue = parseInt(value);
 
-    /* To Do : Add validation */
-    if ((thisWidget.value !== newValue) && !isNaN(newValue) && (newValue <= 10) && (newValue >= 0)) {
-      thisWidget.value = newValue;
-    }
-    // if (thisWidget.value > 10) {
-    //   thisWidget.value = 10;
-    // } else if (thisWidget.value < 0) {
-    //   thisWidget.value = 0;
-    // }
-    // console.log(newValue);
-    thisWidget.input.value = thisWidget.value;
-    // console.log(thisWidget.value);
-    thisWidget.announce();
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
-  initActions(){
+
+  isValid(value) {
+    return !isNaN(value) && value >= settings.amountWidget.defaultMin && value <= settings.amountWidget.defaultMax;
+  }
+
+  renderValue() {
     const thisWidget = this;
-    thisWidget.input.addEventListener('change', function(){
-      thisWidget.setValue(thisWidget.input.value);
+
+    thisWidget.dom.input.value = thisWidget.value;
+  }
+
+  initActions() {
+    const thisWidget = this;
+
+    thisWidget.dom.input.addEventListener('change', function () {
+      // thisWidget.setValue(thisWidget.dom.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
     });
-    thisWidget.linkDecrease.addEventListener('click', function(event){
+
+    thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
       event.preventDefault();
-      thisWidget.setValue(thisWidget.value -= 1);
+      thisWidget.setValue(thisWidget.value - 1);
     });
-    thisWidget.linkIncrease.addEventListener('click', function(event){
-      event.preventDefault();
-      thisWidget.setValue(thisWidget.value += 1);
-    });
-  }
-  announce(){
-    const thisWidget = this;
 
-    const event = new CustomEvent('updated', {
-      bubbles: true,
+    thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
     });
-    thisWidget.element.dispatchEvent(event);
   }
 }
 
